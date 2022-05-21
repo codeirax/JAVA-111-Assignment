1084f7e7be67de9eafe1811f22c84fe7c5e84c58
codeirax <warmachinegaming4@gmail.com>
2022-05-21 23:07:22 +0530

    Added day4 nextMultiple

---
+++ A.java
@@ -0,0 +1,11 @@
+package com.masai;
+
+public class A {
+
+	int  i = 10;  // instance variable non-static mem.
+	
+	void funA() { // Non-static method.
+		System.out.println("Inside funA of A class");
+	}
+	
+}
